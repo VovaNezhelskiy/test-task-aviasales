3e774bcbb242bf8220e353e14cff2935ed091ebf
vovanezha <vova19tup@gmail.com>
2019-09-30 09:52:20 +0300

    Add react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Redirect, Route, Router, Switch } from 'react-router';
 import { configureStore } from './store/configureStore';
 import { Provider } from 'react-redux';
 import { AppContainer } from './modules';
+import { history } from './utils/history';
 
 import './styles/normilaze.css';
 import './styles/colors.css';
@@ -11,7 +13,17 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer />
+    <Router history={history}>
+      <Switch>
+        <Route path="/tickets">
+          <AppContainer />
+        </Route>
+
+        <Route path="/">
+          <Redirect to={"/tickets"} />
+        </Route>
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root'),
 );
